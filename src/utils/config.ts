9ed8177fa063058c0906fb010d4b947d6ddde810
Benny <benjaminvermeulen22@gmail.com>
2025-06-03 16:51:22 -0500

    Add Google Play badge and update config/title/logo

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -57,7 +57,7 @@ const templateConfig: TemplateConfig = {
     subtitle:
       "Unlock the full potential of our services with seamless access at your fingertips. Stay connected, informed, and in control wherever you are.",
     screenshots: [
-      "/screenshots/8.jpg",
+      "/screenshots/v.jpg",
       "/screenshots/2.jpg",
       "/screenshots/3.jpg",
     ],
