68f09e94bfa3a4dc181f2d071c71d1ce2c63b276
Benny <benjaminvermeulen22@gmail.com>
2025-06-03 15:47:22 -0500

    Add Google Play badge and update config/title/logo

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -103,7 +103,7 @@ const templateConfig: TemplateConfig = {
     partners: {
       title: "Get all apps on",
       logos: [
-        "/misc/companies/google.eps",
+        "/misc/companies/google.png",
       ],
     },
     howItWorks: {
